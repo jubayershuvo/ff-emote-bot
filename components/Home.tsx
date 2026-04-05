78a07d88eaa902785a5b950777cb21380cd18268
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2026-04-05 23:45:52 +0600

    sdadasd

--- components/Home.tsx
+++ components/Home.tsx
@@ -345,7 +345,7 @@ export default function HomePage() {
     return () => clearTimeout(timer);
   }, []);
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white p-6">
+    <div className="min-h-screen bg-linear-to-br from-gray-900 via-black to-gray-900 text-white p-6">
       <Toaster position="top-right" reverseOrder={false} />
       <style jsx>{`
         @keyframes float {
